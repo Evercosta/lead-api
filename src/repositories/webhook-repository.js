995a79b4467a0aeac5cc422f3887f07d89dff906
Everton Costa <evercosta@gmail.com>
2020-07-10 16:30:24 -0300

    Teste log

--- src/repositories/webhook-repository.js
+++ src/repositories/webhook-repository.js
@@ -4,5 +4,6 @@ const Webhook = mongoose.model('Webhook');
 
 exports.create = async(data) => {
     var webhook = new Webhook(data);
+    console.log(webhook)
     await webhook.save(); // esse nao precisa retornar
 }
