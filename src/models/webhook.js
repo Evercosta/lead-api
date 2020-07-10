886cfa3780a48e9c713363555f1a090cf9a00176
Everton Costa <evercosta@gmail.com>
2020-07-10 16:42:43 -0300

    Teste log 3

--- src/models/webhook.js
+++ src/models/webhook.js
@@ -17,7 +17,7 @@ const schema = new Schema({
     //     }
     //   }
     time: {
-        type: Number
+        type: String
     },
     changes: {
         type: Array
--- src/repositories/webhook-repository.js
+++ src/repositories/webhook-repository.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const Webhook = mongoose.model('Webhook');
 
 exports.create = async(data) => {
-    console.log(data)
+    console.log('Body antes do squema: ',JSON.stringify(data))
     var webhook = new Webhook(data);
-    
+    console.log('Body depois do squema: ',JSON.stringify(webhook))
     await webhook.save(); // esse nao precisa retornar
 }
